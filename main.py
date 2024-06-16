c59f66e469436180ace8cf67f7fc22430f821d3e
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-06-16 16:03:02 +0200

    Backend cli support

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from pkg.functions import *
 from pkg import PEP
+import argparse
 
-gamma = 1 / 3
-f = SmoothStronglyConvexFunction(3, 1)
-x0 = f.gen_initial_point()
-xs = f.get_stationary_point()
-x = x0
-for _ in range(1):
-    x = x - gamma * f.grad(x)
-pep = PEP(f)
-pep.set_metric((f(xs) - f(x)).abs())
-print(pep.solve(5, backend="Python"))
-""" y = f(xs)
-pep = PEP(f)
-pep.solve(2)
-print(f(x0).eval()) """
+
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--backend", type=str, default="C++")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = cli()
+    n = 4
+    L = 3
+    mu = 1
+    gamma = 1 / L
+    f = SmoothConvexFunction(L)
+    x0 = f.gen_initial_point()
+    xs = f.get_stationary_point()
+    x = x0
+    for _ in range(n):
+        x = x - gamma * f.grad(x)
+    pep = PEP(f)
+    pep.set_metric((f(xs) - f(x)).abs())
+    print(pep.solve(1, backend=args.backend))
+    """ y = f(xs)
+  pep = PEP(f)
+  pep.solve(2)
+  print(f(x0).eval()) """
